5063d234e26736118bc2888d599a89411b89624b
charles66820 <charles.goedefroit@gmail.com>
2019-03-05 06:08:08 +0100

    converter

---
+++ stoko/Class/Converter/noneConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace stoko {
+    class NoneConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return (value.ToString() == "-1") ? null : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return null;
+        }
+    }
+}
---
+++ stoko/Class/Converter/priceConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace stoko {
+    class PriceConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value.ToString() + "€";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return null;
+        }
+    }
+}
